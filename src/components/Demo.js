e4b8a0eb1bf15a0e5f3a20cd9ce8f19ef8f7dd90
zhangmk <51670196@qq.com>
2019-11-01 09:46:49 +0800

    update

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,28 +1,30 @@
 import React from "react";
 import styled, { ThemeProvider } from "styled-components";
-import NoSsr from "@material-ui/core/NoSsr";
-import { createMuiTheme } from "@material-ui/core/styles";
-import { palette, spacing, typography } from "@material-ui/system";
+import Box from "@material-ui/core/Box";
+import { spacing, palette, borders } from "@material-ui/system";
 
-const Box = styled.div`${palette}${spacing}${typography}`;
-// or import Box from '@material-ui/core/Box';
+const Ti = styled.div`${spacing}${palette}${borders}`;
 
-const theme = createMuiTheme();
+const theme = {
+  spacing: 90,
+  palette: {
+    primary: "#007bff"
+  }
+};
 
-export default function Demo() {
+export default function App() {
   return (
-    <NoSsr>
+    <>
       <ThemeProvider theme={theme}>
-        <Box
-          color="primary.main"
-          bgcolor="background.paper"
-          fontFamily="h6.fontFamily"
-          fontSize={{ xs: "h6.fontSize", sm: "h4.fontSize", md: "h3.fontSize" }}
-          p={{ xs: 2, sm: 3, md: 4 }}
-        >
-          @material-ui/system
-        </Box>
+        {/* children ddd*/}
+        <Ti p={1} border={3} borderColor="primary" color="primary">
+          4px
+        </Ti>
+        {/* <Box p={1} border={10} borderColor="blue" color="pinks">
+          fef
+        </Box> */}
+        <Box p={3} color="white" bgcolor="black" fontSize={100}></Box>
       </ThemeProvider>
-    </NoSsr>
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./app";
+import App from "./app";
 // import Capsule from "./components/Capsule";
 import Button from "@material-ui/core/Button";
 // import Box from "@material-ui/core/Box";
 import Box from "./components/Box";
 import Demo from "./components/Demo";
 
-const Big = () => {
-  return (
-    <>
-      <Box
-        // textAlign={{ xs: "right", md: "center" }}
-        color="text.primary"
-        bgcolor="secondary.main"
-        m={0} // margin
-        // p={0}
-        clone
-      >
-        <Button>111</Button>
-        {/* <Button>222</Button> */}
-      </Box>
+// const Big = () => {
+//   return (
+//     <>
+//       <Box
+//         // textAlign={{ xs: "right", md: "center" }}
+//         color="text.primary"
+//         bgcolor="secondary.main"
+//         m={0} // margin
+//         // p={0}
+//         clone
+//       >
+//         <Button>111</Button>
+//         {/* <Button>222</Button> */}
+//       </Box>
 
-      <Box
-        // textAlign={{ xs: "right", md: "center" }}
-        color="text.primary"
-        bgcolor="secondary.main"
-        m={2} // margin
-        clone
-      >
-        <Button>222</Button>
-        {/* <Button>222</Button> */}
-      </Box>
-      <Demo></Demo>
-    </>
-  );
-};
+//       <Box
+//         // textAlign={{ xs: "right", md: "center" }}
+//         color="text.primary"
+//         bgcolor="secondary.main"
+//         m={2} // margin
+//         clone
+//       >
+//         <Button>222</Button>
+//         {/* <Button>222</Button> */}
+//       </Box>
+//       <Demo></Demo>
+//     </>
+//   );
+// };
 
-ReactDOM.render(<Big />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
